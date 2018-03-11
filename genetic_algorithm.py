caf232b5437c6327b5d3dbba267aaede4a9be90e
Tomasz Cybulski <tomcyb27@gmail.com>
2018-03-11 15:56:28 +0100

    Refactoring and optimization

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -12,6 +12,8 @@ class GeneticAlgorithm:
         self.__mutation_probability = mutation_probability
         self.__tour = tour
         self.__crossover_probability = crossover_probability
+        self.__population = np.empty()
+        self.__population_fitness = np.empty()
 
         if selection_method.lower() == 'tournament':
             self.__selection_method = self.__tournament_selection
@@ -20,28 +22,36 @@ class GeneticAlgorithm:
         else:
             raise Exception('Not supported selection method')
 
+    def initialise_population(self):
+        self.__population = np.array([self.__generate_specimen() for _ in range(0, self.__pop_size)])
+
+    def __generate_specimen(self):
+        specimen = [i for i in range(1, self.__num_of_locations + 1)]
+        np.random.shuffle(specimen)
+        return specimen
+
     def run(self, generations):
         current_generation = 1
-        population = self.initialise_population()
-        population_fitness = self.evaluate(population)
+        self.initialise_population()
+        self.__evaluate()
 
-        current_best_fitness, current_best_specimen = self.__get_current_best(population, population_fitness)
+        current_best_specimen, current_best_fitness = self.__get_current_best()
         global_best_specimen, global_best_fitness = current_best_specimen, current_best_fitness
 
-        self.__data_logger.write_log(current_generation, current_best_fitness, np.average(population_fitness),
-                                     np.amax(population_fitness))
+        self.__data_logger.write_log(current_generation, current_best_fitness, np.average(self.__population_fitness),
+                                     np.amax(self.__population_fitness))
 
         while current_generation < generations:
-            population = self.selection(population, population_fitness)
-            population = self.crossover(population)
-            self.mutation(population)
+            self.__selection()
+            self.__crossover()
+            self.__mutation()
 
-            population_fitness = self.evaluate(population)
+            self.__evaluate()
 
-            current_best_fitness, current_best_specimen = self.__get_current_best(population, population_fitness)
+            current_best_specimen, current_best_fitness = self.__get_current_best()
             current_generation += 1
-            self.__data_logger.write_log(current_generation, current_best_fitness, np.average(population_fitness),
-                                         np.amax(population_fitness))
+            self.__data_logger.write_log(current_generation, current_best_fitness, np.average(self.__population_fitness),
+                                         np.amax(self.__population_fitness))
 
             if current_best_fitness < global_best_fitness:
                 global_best_fitness = current_best_fitness
@@ -49,56 +59,12 @@ class GeneticAlgorithm:
 
         return global_best_specimen, global_best_fitness
 
-    def __get_current_best(self, population, population_fitness):
-        current_best_index = np.argmin(population_fitness)
-        current_best_specimen = population[current_best_index]
-        current_best_fitness = population_fitness[current_best_index]
-        return current_best_fitness, current_best_specimen
+    def __get_current_best(self):
+        current_best_index = np.argmin(self.__population_fitness)
+        return self.__population[current_best_index], self.__population_fitness[current_best_index]
 
-    def evaluate(self, population):
-        return np.array(
-            [self.__evaluate_specimen_fitness(specimen) for specimen in population]
-        )
-
-    def selection(self, population, population_fitness):
-        return self.__selection_method(population, population_fitness)
-
-    def __tournament_selection(self, population, population_fitness):
-        selected_population = []
-        for i in range(0, self.__pop_size):
-            competitors_indices = [np.random.randint(0, len(population)) for _ in range(0, self.__tour)]
-            best_competitor_index = np.argmin(population_fitness[competitors_indices])
-            selected_population.append(population[best_competitor_index])
-        return np.array(selected_population)
-
-    def crossover(self, population):
-        children = []
-
-        for i in range(0, self.__pop_size):
-            first_parent_index = np.random.randint(0, len(population))
-            second_parent_index = np.random.randint(0, len(population))
-            first_child, second_child = self.__specimens_crossover(population[first_parent_index],
-                                                                   population[second_parent_index])
-            children.extend([first_child, second_child])
-        return np.array(children)
-
-    def mutation(self, population):
-        for specimen in population:
-            if np.random.random() < self.__mutation_probability:
-                first_index = np.random.randint(0, len(specimen))
-                second_index = (first_index + np.random.randint(1, len(specimen))) % len(specimen)
-                temp = specimen[first_index]
-                specimen[first_index] = specimen[second_index]
-                specimen[second_index] = temp
-
-    def initialise_population(self):
-        population = np.array([self.__generate_specimen() for _ in range(0, self.__pop_size)])
-        return population
-
-    def __generate_specimen(self):
-        specimen = [i for i in range(1, self.__num_of_locations + 1)]
-        np.random.shuffle(specimen)
-        return specimen
+    def __evaluate(self):
+        self.__population_fitness = np.array([self.__evaluate_specimen_fitness(specimen) for specimen in self.__population])
 
     def __evaluate_specimen_fitness(self, specimen):
         fitness_acc = 0
@@ -110,6 +76,43 @@ class GeneticAlgorithm:
                     specimen[facility] - 1,
                     specimen[other_facility] - 1]
         return fitness_acc
+
+    def __selection(self):
+        self.__population = self.__selection_method()
+
+    def __tournament_selection(self):
+        selected_population = []
+        for i in range(0, self.__pop_size):
+            competitors_indices = [np.random.randint(0, self.__pop_size) for _ in range(0, self.__tour)]
+            best_competitor_index = np.argmin(self.__population_fitness[competitors_indices])
+            selected_population.append(self.__population[best_competitor_index])
+        return np.array(selected_population)
+
+    def __roulette_selection(self):
+        #do optymalizacji
+        pop_fitness = self.__population_fitness
+        fitness_sum = np.sum(pop_fitness)
+        selected_population = []
+        for i in range(0, self.__pop_size):
+            random_selection = np.random.randint(0, fitness_sum + 1)
+            specimen_index = 0
+            accumulator = pop_fitness[specimen_index]
+            while accumulator < random_selection:
+                specimen_index += 1
+                accumulator += pop_fitness[specimen_index]
+            selected_population.append(self.__population[specimen_index])
+
+        return np.array(selected_population)
+
+    def __crossover(self):
+        children = []
+        for i in range(0, self.__pop_size, 2):
+            first_parent_index = np.random.randint(0, self.__pop_size)
+            second_parent_index = np.random.randint(0, self.__pop_size)
+            first_child, second_child = self.__specimens_crossover(self.__population[first_parent_index],
+                                                                   self.__population[second_parent_index])
+            children.extend([first_child, second_child])
+        self.__population = np.array(children)
 
     def __specimens_crossover(self, first_parent, second_parent):
         if np.random.randint(0, 1) >= self.__crossover_probability:
@@ -139,16 +142,15 @@ class GeneticAlgorithm:
             specimen[i] = not_found[0]
             del not_found[0]
 
-    def __roulette_selection(self, population, population_fitness):
-        fitness_sum = np.sum(population_fitness)
-        selected_population = []
-        for i in range(0, self.__pop_size):
-            random_selection = np.random.randint(0, fitness_sum + 1)
-            specimen_index = 0
-            accumulator = population_fitness[specimen_index]
-            while accumulator < random_selection:
-                specimen_index += 1
-                accumulator += population_fitness[specimen_index]
-            selected_population.append(population[specimen_index])
+    def __mutation(self):
+        for specimen in self.__population:
+            if np.random.random() < self.__mutation_probability:
+                first_index = np.random.randint(0, len(specimen))
+                second_index = (first_index + np.random.randint(1, len(specimen))) % len(specimen)
+                temp = specimen[first_index]
+                specimen[first_index] = specimen[second_index]
+                specimen[second_index] = temp
 
-        return np.array(selected_population)
+
+
+
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from data_logger import DataLogger
 from data_reader import *
 from genetic_algorithm import *
 
-pop_size = 100
-gen = 100
+pop_size = 1000
+gen = 1000
 Px = 0.7
 Pm = 0.01
 Tour = 5
@@ -12,6 +12,6 @@ reader = DataReader()
 n, flow_matrix, distance_matrix = reader.read_data("data/had12.dat")
 logger = DataLogger()
 
-genetic_algorithm = GeneticAlgorithm(n, pop_size, flow_matrix, distance_matrix, Pm, Px, Tour, logger, 'roulette')
+genetic_algorithm = GeneticAlgorithm(n, pop_size, flow_matrix, distance_matrix, Pm, Px, Tour, logger)
 print(genetic_algorithm.run(gen))
 logger.close()
