9707a6dfda5fab89efa7c78dbb388007358b18bd
Tomasz Cybulski <tomcyb27@gmail.com>
2018-03-11 17:23:43 +0100

    Testing

--- data_logger.py
+++ data_logger.py
@@ -4,13 +4,15 @@ from datetime import datetime
 class DataLogger:
     __timestamp_format = '%Y_%m_%d_%H_%M_%S'
 
-    def __init__(self):
-        self.__file = open('out/ga_result_%s.csv' %
-                           datetime.now().strftime(self.__timestamp_format), 'w')
-        self.__file.write('nr_pokolenia, najlepsza_ocena, srednia_ocen, najgorsza_ocena\n')
+    def __init__(self, title_prefix=''):
+        self.__file = open('out/%sga_result_%s.csv' % (title_prefix + '_' if title_prefix != '' else '',
+                           datetime.now().strftime(self.__timestamp_format)), 'w')
 
     def write_log(self, gen_nr, best, average, worst):
         self.__file.write('%s,%s,%s,%s\n' % (gen_nr, best, average, worst))
+
+    def write_header(self):
+        self.__file.write('nr_pokolenia, najlepsza_ocena, srednia_ocen, najgorsza_ocena\n')
 
     def close(self):
         self.__file.close()
--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from data_reader import *
 from genetic_algorithm import *
 
 pop_size = 100
-gen = 1000
+gen = 100
 Px = 0.7
 Pm = 0.01
 Tour = 5
 
-reader = DataReader()
-n, flow_matrix, distance_matrix = reader.read_data("data/had12.dat")
-logger = DataLogger()
 
-genetic_algorithm = GeneticAlgorithm(n, pop_size, flow_matrix, distance_matrix, Pm, Px, Tour, logger, 'roulette')
-print(genetic_algorithm.run(gen))
-logger.close()
+for i in range(12, 21, 2):
+    reader = DataReader()
+    n, flow_matrix, distance_matrix = reader.read_data("data/had%s.dat" % str(i))
+    for j in range(0, 20):
+        logger = DataLogger('had%s' % str(i))
+        logger.write_header()
+        genetic_algorithm = GeneticAlgorithm(n, pop_size, flow_matrix, distance_matrix, Pm, Px, Tour, logger, )
+        print('Had%s file best result: %s' % (i, genetic_algorithm.run(gen)))
+        logger.close()
+
+
+
